cbd71607376918e9817f451385107073acd7d12c
Dhruv Gera <42107491+Dhruvgera@users.noreply.github.com>
2020-02-14 17:51:48 +0530

    Correct order of commands

--- personal/setup.sh
+++ personal/setup.sh
@@ -11,5 +11,5 @@ sudo apt-get install cpufrequtils
 echo "sudo cpufreq-set -r -g performance" >> .bashrc
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key add -
 sudo apt update && sudo apt install linux-xanmod
-sudo reboot
 ccache -M 100G
+sudo reboot
