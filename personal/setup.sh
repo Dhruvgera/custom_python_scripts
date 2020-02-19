5073edeae603a1fe319431f4d9cba2a16295453f
Dhruv Gera <42107491+Dhruvgera@users.noreply.github.com>
2020-02-20 00:56:12 +0530

    Remove git config

--- personal/setup.sh
+++ personal/setup.sh
@@ -1,5 +1,3 @@
-git config --global user.email dhruvgera61@gmail.com 
-git config --global user.name dhruv 
 git clone https://github.com/akhilnarang/scripts 
 cd scripts 
 bash setup/android_build_env.sh 
