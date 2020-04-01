d28dae858cf641dff0e5a0a383cddad1a3142411
Dhruv Gera <42107491+Dhruvgera@users.noreply.github.com>
2020-04-01 23:51:46 +0530

    CI: Use function to check for existence of zip

--- CI/main.sh
+++ CI/main.sh
@@ -16,14 +16,16 @@ python3 $HOME/cygnus/replacer.py
 wait
 bash $HOME/cygnus/start.sh
 wait
-ZIP=$HOME/cygnus/*zip
-if [[ ! -f "${ZIP}" ]]; then
+cd $HOME/cygnus/
+function globexists {
+   test -e "$1" -o -L "$1"
+}
+if ! globexists Cygnus* ; then                          
    echo -e "Build failed :p";
    function SendMsg() {  
    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d "parse_mode=markdown" -d text="$1" -d chat_id=$CHAT_ID 1> /dev/null 
    }
    SendMsg "Build has failed! Check the logs please";
-   cd $HOME/cygnus
    grep -iE 'crash|error|fail|fatal' "log.txt" &> "trimmed.txt"
    curl -F chat_id="$CHAT_ID" -F document=@"trimmed.txt" -F caption="Woah, I trimmed them for you" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
    cd $HOME/cygnus/scripts/CI/
