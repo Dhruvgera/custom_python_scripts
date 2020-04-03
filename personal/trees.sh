42c03f3b3ea70b00942f7ea3a1787429b4f70b84
Dhruv <dhruvgera61@gmail.com>
2020-04-03 19:43:12 +0530

    personal: Update branches
    Signed-off-by: Dhruvgera <dhruvgera61@gmail.com>

--- personal/trees.sh
+++ personal/trees.sh
@@ -1,17 +1,17 @@
-cd ../..
+cd $HOME/cygnus/
 git clone https://github.com/Dhruvgera/device_xiaomi_beryllium.git  -b cygnus-caf device/xiaomi/beryllium
 cd device/xiaomi/beryllium
-git push -f https://github.com/Cygnus-devices/device_xiaomi_beryllium.git HEAD:ten
+git push -f https://github.com/Cygnus-devices/device_xiaomi_beryllium.git HEAD:caf-ten
 cd ../../..
 git clone https://github.com/Dhruvgera/beryllium.git -b cygnus-caf device/xiaomi/sdm845-common
 cd device/xiaomi/sdm845-common
-git push -f https://github.com/Cygnus-devices/device_xiaomi_sdm845-common.git HEAD:ten
+git push -f https://github.com/Cygnus-devices/device_xiaomi_sdm845-common.git HEAD:caf-ten
 cd ../../..
 git clone https://github.com/Dhruvgera/vendor_xiaomi_beryllium.git vendor/xiaomi -b rebase
 cd vendor/xiaomi
-git push -f https://github.com/Cygnus-devices/vendor_xiaomi_beryllium.git HEAD:ten
+git push -f https://github.com/Cygnus-devices/vendor_xiaomi_beryllium.git HEAD:caf-ten
 cd ../..
 git clone https://github.com/Dhruvgera/RockstarKernel_beryllium.git kernel/xiaomi/beryllium
 cd kernel/xiaomi/beryllium
-git push https://github.com/Cygnus-devices/kernel_xiaomi_beryllium.git HEAD:ten
+git push https://github.com/Cygnus-devices/kernel_xiaomi_beryllium.git HEAD:caf-ten
 cd ../..
