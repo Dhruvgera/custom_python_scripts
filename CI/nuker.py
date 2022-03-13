c81cd7dcd1841665672ff6e69da2160c5eb9525d
Dhruv Gera <dhruvgera61@gmail.com>
2022-03-13 18:56:53 +0530

    CI: Actually nuke stuff

--- CI/nuker.py
+++ CI/nuker.py
@@ -1,4 +1,4 @@
-import json,glob
+import json,glob,os
 
 device_name = "device_name_here"
 
@@ -8,6 +8,6 @@ def nuke():
     with open(path_to_dep) as file:
         json_data = json.loads(file.read())
         for repos in json_data:
-            print(f"rm -rf {repos['target_path']}")
+            os.system(f"rm -rf {repos['target_path']}")
 
 nuke()
