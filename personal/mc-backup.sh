d19eccfad054bf20c4063840a72d52efb6c52831
Dhruv Gera <dhruvgera61@gmail.com>
2022-03-15 12:23:53 +0530

    mc-backup: Switch to Git LFS

--- personal/mc-backup.sh
+++ personal/mc-backup.sh
@@ -9,13 +9,22 @@ cd backups
 sudo rm -rf .git
 git init
 
+# Add a README files
+echo "Backups for minecraftbutcursed.com world" > README.md
+
 # Zip all the world folders into a file
 zip -r backup.zip *
+
+# Do Git LFS thingies
+git lfs install
+git lfs track backup.zip
+git add .gitattributes
+git add README.md
 git add backup.zip
 git commit -m "New Backup"
 
 # Force push the file
-git push -f git@gitlab.com:Dhruvgera/mc-backups.git HEAD:main
+git push -f git@github.com:Dhruvgera/mc-backups.git HEAD:main
 cd $HOME
 
 # Clean up what you did
